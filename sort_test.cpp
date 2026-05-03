4c230ef63e2be360836f458baf30e5059bf94029
Kirill Zimogliad <kirill06052007@gmail.com>
2026-05-04 00:09:34 +0300

    Add files via upload

---
+++ sort_test.cpp
@@ -0,0 +1,11 @@
+#include <gtest/gtest.h>
+
+// А вот тут collvalue.h не надо, берите просто инты
+#include "sorting.h"
+
+// Но проверьте, что сортировки таки работают...
+
+TEST(FooSuite, FooTest)
+{
+    EXPECT_EQ(0, 0);
+}
---
+++ sorting.h
@@ -0,0 +1,21 @@
+#ifndef SORTING_H
+#define SORTING_H
+
+// Header-only
+
+// Для swap
+#include "collvalue.h"
+
+// ----------------------------- Пузырёк (например) --------------------------------------
+template <typename Iterator>
+void bubble_sort(Iterator begin, Iterator end)
+{
+}
+
+// --------------------------- QuickSort (например) ---------------------------------------
+template <typename Iterator>
+void quick_sort(Iterator begin, Iterator end)
+{
+}
+
+#endif // SORTING_H
